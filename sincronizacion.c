d5cd70f02a9a3481f08e96101f7c421f4a5cf603
asorianog <asorianog@icloud.com>
2015-11-13 12:55:00 -0600

    nuevo

---
+++ sincronizacion.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<wait.h>
+
+int procesos(int nProc){
+	int i;
+	for(i = 1; i<=nProc; i++)
+		if(fork() == 0)
+			return i;
+		return 0;
+}
+
+int main(){
+	int pid,num,j;
+	int status;
+	
+	printf("Numero de procesos \n");
+	scanf("%d",&num);
+	pid = procesos(num);
+
+
+	if(pid == 0){
+	
+	for(j = 0; j <= num; j++){
+	wait(&status);
+	
+	}
+	printf("Soy el padre %d con pid%d \n", pid,getpid());
+	exit(0);
+
+	}else{
+
+	printf("Soy el hijo %d con pid%d \n", pid,getpid());
+	exit(0);
+
+	}
+	return 0;
+}
+	
